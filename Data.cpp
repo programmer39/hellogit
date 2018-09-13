affecb88d832d808e85c253d1a5d135d6fb1c319
programmer37 <programmer39@users.noreply.github.com>
2018-09-13 14:54:39 -0400

    Part of Lab1

---
+++ Data.cpp
@@ -0,0 +1,28 @@
+#include "Data.h"
+
+	Data::Data()
+	{
+		x=0;
+		y=0.0;
+	}
+	
+	Data::Data (int a, float b){
+		x=a;
+		y=b;		}
+
+	int Data::getX(){
+		return x;
+		}
+
+	float Data:getY(){
+		return y;
+		}
+	
+	void Data::SetX(int a){
+		x=a;		
+			}
+	
+	void Data::SetY(float b){
+		y=b;
+			}
+	
---
+++ Data.h
@@ -0,0 +1,16 @@
+#ifndef Data_H
+#define Data_H
+class Data{
+	private:
+		int x;
+		float y;
+
+	public:
+		Data()
+		Data(inat a, float b);
+		int getX();
+		float getY();
+		void setX(int a);
+		void setY(float b);
+	};
+#endif
